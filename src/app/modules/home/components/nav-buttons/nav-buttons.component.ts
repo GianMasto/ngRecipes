bf8694e4ec97b97efb110fffb55c20fc068cbc66
Gian Masto <gianmasto1@gmail.com>
2023-11-27 17:56:05 -0300

    dynamic nav buttons

--- src/app/modules/home/components/nav-buttons/nav-buttons.component.ts
+++ src/app/modules/home/components/nav-buttons/nav-buttons.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-buttons',
   templateUrl: './nav-buttons.component.html',
-  styleUrls: ['./nav-buttons.component.css']
+  styleUrls: ['./nav-buttons.component.css'],
 })
 export class NavButtonsComponent {
-
+  @Input() buttons!: { label: string; path: string }[];
 }
--- src/app/modules/home/pages/home-page/home-page.component.ts
+++ src/app/modules/home/pages/home-page/home-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
-export class HomePageComponent {
-
+export class HomePageComponent implements OnInit {
+  buttons!: { label: string; path: string }[];
+  ngOnInit(): void {
+    this.buttons = [
+      {
+        label: 'Recipes',
+        path: '/',
+      },
+      {
+        label: 'Favorites',
+        path: '/favorites',
+      },
+      {
+        label: 'Shopping List',
+        path: '/shopping-list',
+      },
+    ];
+  }
 }
